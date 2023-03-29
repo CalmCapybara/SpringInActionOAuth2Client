ff8202cb95f5cd25eeee0a46f6555338413c0c1c
Kitavtsev Stanislav <Kitavtsev.s@gmail.com>
2023-03-29 22:48:13 +0300

    init commit

---
+++ src/main/java/com/calmcapybara/springinactionclient/SpringInActionClientApplication.java
@@ -0,0 +1,13 @@
+package com.calmcapybara.springinactionclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInActionClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInActionClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calmcapybara/springinactionclient/SpringInActionClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calmcapybara.springinactionclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
